c55a059272780e7411a7b7b768af1b363e5a668a
Scharkee <matas2k@gmail.com>
2017-03-24 22:20:40 +0200

    UI restruct + new additions to UI Inventory values

---
+++ emporium/Assets/Scripts/DisabledObjectsGameScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisabledObjectsGameScene : MonoBehaviour {
+
+    public static GameObject PressContextPanel;
+    public static GameObject LoadingPanel;
+    public static GameObject Inventory_Fruit_panel;
+
+    // Use this for initialization
+    void Start () {
+       // Inventory_Fruit_panel.SetActive(false);  TODO: should disable this but without breaking data transfer
+
+    }
+    void Awake()
+    {
+        PressContextPanel = GameObject.Find("PressContextPanel");
+        LoadingPanel = GameObject.Find("LoadingPanel");
+        Inventory_Fruit_panel = GameObject.Find("Inventory_Fruit_panel");
+
+
+
+    }
+
+
+
+}
---
+++ emporium/Assets/Scripts/InvExpander.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvExpander : MonoBehaviour {
+    
+
+    void Start()
+    {
+
+
+    }
+
+ 
+
+
+    public void expandContract()
+    {
+
+        if (DisabledObjectsGameScene.Inventory_Fruit_panel.activeSelf)
+        {
+            DisabledObjectsGameScene.Inventory_Fruit_panel.SetActive(false);
+        }else
+        {
+            DisabledObjectsGameScene.Inventory_Fruit_panel.SetActive(true);
+        }
+    }
+
+}
