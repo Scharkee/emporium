33d07e0944dd0b070c41e1a8631d57d91a59e838
Scharkee <matas2k@gmail.com>
2017-05-01 22:31:55 +0300

    minor changes;

--- emporium/Assets/Scripts/MenuMusic.cs
+++ emporium/Assets/Scripts/MenuMusic.cs
@@ -30,17 +30,24 @@ public class MenuMusic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+    }
+
+    void FixedUpdate()
+    {
+
         float[] spectrum = audiosrc.GetSpectrumData(1024, 1, FFTWindow.BlackmanHarris);// you can use other calculations than blackmanharris.
 
         spectrumScale = 1f + ((spectrum[specStat1] + spectrum[specStat2]) * curveEnhancer);
 
-        DisabledObjectsMain.Instance.titleText.transform.localScale = Vector3.Lerp(DisabledObjectsMain.Instance.titleText.transform.localScale, new Vector3(Mathf.Clamp(spectrumScale, 1,1.3f) , Mathf.Clamp(spectrumScale, 1, 1.3f), Mathf.Clamp(spectrumScale, 1, 1.3f) ), 0.01f);
-        Camera.main.GetComponent<Fisheye>().strengthX = Mathf.Lerp(Camera.main.GetComponent<Fisheye>().strengthX, (spectrumScale - 1) / 2, 0.2f);
-        Camera.main.GetComponent<Fisheye>().strengthY = Mathf.Lerp(Camera.main.GetComponent<Fisheye>().strengthY, (spectrumScale - 1) / 2, 0.2f);
+        DisabledObjectsMain.Instance.titleText.transform.localScale = Vector3.Lerp(DisabledObjectsMain.Instance.titleText.transform.localScale, new Vector3(Mathf.Clamp(spectrumScale, 1, 1.3f), Mathf.Clamp(spectrumScale, 1, 1.3f), Mathf.Clamp(spectrumScale, 1, 1.3f))* spectrumScale, Time.deltaTime*spectrumScale);
+        Camera.main.GetComponent<Fisheye>().strengthX = Mathf.Lerp(Camera.main.GetComponent<Fisheye>().strengthX, (spectrumScale - 1) / 2, 0.3f);
+        Camera.main.GetComponent<Fisheye>().strengthY = Mathf.Lerp(Camera.main.GetComponent<Fisheye>().strengthY, (spectrumScale - 1) / 2, 0.3f);
 
-      
+
         DisabledObjectsMain.Instance.titleText.GetComponent<TitleText>().colorChangeAdditive = (spectrumScale - 1) * 2;
-        
+
     }
 
 
--- emporium/Assets/Scripts/TitleText.cs
+++ emporium/Assets/Scripts/TitleText.cs
@@ -30,7 +30,7 @@ public class TitleText : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
 
