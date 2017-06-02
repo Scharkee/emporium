c9d55996098bee18facbcbf9cd85333ebfa0d81e
Scharkee <matas2k@gmail.com>
2017-06-02 16:36:23 +0300

    Back to dev; started buy menu grid overhaul; increased resolution of various text items to improve readability;

--- emporium/Assets/Scripts/BuyMenuInfoLoader.cs
+++ emporium/Assets/Scripts/BuyMenuInfoLoader.cs
@@ -20,72 +20,44 @@ public class BuyMenuInfoLoader : MonoBehaviour
     {
         foreach (Building building in Database.Instance.buildinginfo)
         {
-            if (building.BUILDING_TYPE == 0)
+            if (building.BUILDING_TYPE == 0)//augalas
             {
-                try
-                {
-                    GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
-                    TimeSpan ts = TimeSpan.FromSeconds(building.PROG_AMOUNT);
+                GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
+                TimeSpan ts = TimeSpan.FromSeconds(building.PROG_AMOUNT);
 
-                    GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = string.Format("{0:D2}:{1:D2}:{2:D2}", ts.Hours, ts.Minutes, ts.Seconds);
-                    GameObject.Find("OPGridText_" + building.NAME + "_efic").GetComponent<Text>().text = "Yield: ";
-                    GameObject.Find("OPGridText_" + building.NAME + "_efic_editable").GetComponent<Text>().text = building.TILEPRODUCERANDOM1 + " - " + building.TILEPRODUCERANDOM2 + " KG";
-                }
-                catch
-                {
-                }
+                GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = string.Format("{0:D2}:{1:D2}:{2:D2}", ts.Hours, ts.Minutes, ts.Seconds);
+                GameObject.Find("OPGridText_" + building.NAME + "_efic").GetComponent<Text>().text = "Yield: ";
+                GameObject.Find("OPGridText_" + building.NAME + "_efic_editable").GetComponent<Text>().text = building.TILEPRODUCERANDOM1 + " - " + building.TILEPRODUCERANDOM2 + " KG";
             }
-            else if (building.BUILDING_TYPE == 1)
+            else if (building.BUILDING_TYPE == 1) //presas
             {
-                try
-                {
-                    GameObject.Find("OPGridText_" + building.NAME + "_efic_editable").GetComponent<Text>().text = building.EFIC * 100 + "%";
-                    GameObject.Find("OPGridText_" + building.NAME + "_efic").GetComponent<Text>().text = "Efficiency:";
+                GameObject.Find("OPGridText_" + building.NAME + "_efic_editable").GetComponent<Text>().text = building.EFIC * 100 + "%";
+                GameObject.Find("OPGridText_" + building.NAME + "_efic").GetComponent<Text>().text = "Efficiency:";
 
-                    GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
-                    GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = "1 KG/s.";
-                }
-                catch
-                {
-                }
+                GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
+                GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = "1 KG/s.";
             }
-            else if (building.BUILDING_TYPE == 2)
+            else if (building.BUILDING_TYPE == 2) //transporto priemone
             {
-                try
-                {
-                    GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
-                    GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " seconds per job.";
-                    GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Job time:";
-                }
-                catch
-                {
-                }
+                GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
+                GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " seconds per job.";
+                GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Job time:";
             }
-            else if (building.BUILDING_TYPE == 3) //TODO: fix visa sita bloka, perdaryt buyPanelButtonus kad butu text1 text2 kuriuos galima keisti pagal BUILDING_TYPE
+            else if (building.BUILDING_TYPE == 3) //solid storage
             {
-                try
-                {
-                    GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
-                    GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " KG.";
-                    GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Capacity:";
-                }
-                catch
-                {
-                }
+                GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
+                GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " KG.";
+                GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Capacity:";
             }
-            else if (building.BUILDING_TYPE == 4)
+            else if (building.BUILDING_TYPE == 4)//liquid storage
             {
-                try
-                {
-                    GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
-                    GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " L.";
-                    GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Capacity:";
-                }
-                catch
-                {
-                }
+                GameObject.Find("OPGridText_" + building.NAME + "_price_editable").GetComponent<Text>().text = building.PRICE.ToString();
+                GameObject.Find("OPGridText_" + building.NAME + "_time_editable").GetComponent<Text>().text = building.PROG_AMOUNT + " L.";
+                GameObject.Find("OPGridText_" + building.NAME + "_time").GetComponent<Text>().text = "Capacity:";
             }
         }
+
+        //finishing up
         DisabledObjectsGameScene.Instance.gridBuildings.SetActive(false);
         DisabledObjectsGameScene.Instance.BuyMenuPanel.SetActive(false);
     }
